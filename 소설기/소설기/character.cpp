85643f05119b6a3d14d2ee1ccfa97a1793c138dc
hakdong-king <tmddnr040516@gmail.com>
2024-10-29 23:28:00 +0900

    충돌치리 및 중력 함수 추가 ,screen 변수와  천장에 충돌 시 처리하는 부분은 아직 추가하지 않음

--- 소설기/소설기/character.cpp
+++ 소설기/소설기/character.cpp
@@ -7,13 +7,21 @@
 character::character()
 {
 	x = 0, y = 0;
+	
 }
 
 void character::characterMove(int key)
 {
+
 	if (key == 32)
 	{
+		
+	
+		
 		y += blockSize + 10;
+		if (collision() == 3) {
+			
+		}
 	}
 
 	if (key == 224)
@@ -26,13 +34,55 @@ void character::characterMove(int key)
 			// 문에서 들어가기
 			break;
 		case 75:
+			if (collision() == 2) {
+				int next_x = x - blockSize;
+				if (next_x <0)
+					break;
+			}
+
+
+			
 			x -= blockSize;
 			break;
 		case 77:
+			if (collision() == 2) {
+				int next_x = x + blockSize;
+				if (next_x > 1800)
+					break;
+			}
+
 			x += blockSize;
 			break;
 		default:
 			break;
 		}
 	}
+}
+void character::gravity() {
+	
+	while (1) {
+		
+		if (collision()==1)
+			break;
+		y--;
+	}
+}
+int character::collision() {
+	for (int i = 0; i < 1800; i++) {
+		for (int j = 0; j < 500; j++) {
+			if (player[j][i] == 1) {
+				int newX = x + i;
+				int newY = y + j;
+
+				if (screen[newY][newX] == 1) {
+					return 1;		//바닥에 충돌
+				}
+				else if (screen[newY][newX] == 2)
+					return 2;		//벽에 충돌
+				else if (screen[newY][newX] == 3)
+					return 3;		//천장에 충돌
+			}
+		}
+	}
+		
 }
--- 소설기/소설기/character.h
+++ 소설기/소설기/character.h
@@ -3,7 +3,9 @@ class character
 {
 	int x;
 	int y;
-
+	int player[500][1800];
 	character();
 	void characterMove(int key);
+	int collision();
+	void gravity();
 };
