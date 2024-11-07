ef69b94a7c98b9fc1160406837eb54d98f14f5a2
psparrow3 <psparrow3@naver.com>
2024-11-07 21:29:28 +0900

    no message

--- 소설기/소설기/main.cpp
+++ 소설기/소설기/main.cpp
@@ -7,32 +7,20 @@
 #include "drawCharacter.h"
 #include "conio.h"
 
-int stage_prologue[24][40]
+int stage1_past[12][20] =
 {
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,9,9,0,0,0,0,0,0,0},
-   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{0,0,0,0,0,0,0,0,3,0,3,0,0,0,0,0,0,0,0,0},
+	{0,0,2,0,0,0,0,1,1,1,1,1,0,0,0,0,0,0,1,1},
+	{1,1,0,0,0,1,1,1,1,1,1,1,1,7,0,0,0,8,1,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,0},
+	{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+	{1,1,0,0,1,1,0,1,1,0,0,0,0,0,0,0,0,0,5,0},
+	{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1},
+	{0,0,0,1,1,0,0,0,0,0,0,0,6,0,3,0,0,1,0,0},
+	{0,0,0,0,1,0,0,0,0,0,0,0,0,1,1,1,1,7,0,0},
+	{0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,0,0,1,1},
+	{0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
 };
 
 int main() {
@@ -41,62 +29,58 @@ int main() {
 	a.SetConsoleFontSize(1);
 	SetConsoleTitle(L"잃어버린 낙원");
 
-
-	// 콘솔 창 설정
-	a.SetConsoleSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-	a.SetConsoleFontSize(1);
-	SetConsoleTitle(L"잃어버린 낙원");
-
-
+	// 콘솔 커서 숨기기
 	HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
 	CONSOLE_CURSOR_INFO curCursorInfo;
 	GetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
-	curCursorInfo.bVisible = 0; // 커서 숨기기
+	curCursorInfo.bVisible = 0;
 	SetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
 
-	// 화면 버퍼 생성 및 초기화
+	// 화면 버퍼 생성
 	std::vector<char> buffer(SCREEN_WIDTH * SCREEN_HEIGHT, ' ');
 
-	// 처음에 맵을 그려놓고, 이를 한 번에 표시
-	int x, y;
-	for (x = 0; x < SCREEN_WIDTH - BLOCK_SIZE * 10; x += BLOCK_SIZE * 2) {
-		a.drawBitmap("block.bmp", buffer, x, 440, SCREEN_WIDTH);
+	// stage1_pre 배열 기반으로 맵을 그림
+	for (int y = 0; y < 12; y++) {
+		for (int x = 0; x < 20; x++) {
+			switch (stage1_past[y][x]) {
+			case 1:
+				a.drawBitmap("block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 2:
+				a.drawBitmap("lava2.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 3:
+				a.drawBitmap("movable_block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 4:
+				a.drawBitmap("key.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 5:
+				a.drawBitmap("button1.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE + 20, SCREEN_WIDTH);
+				break;
+			case 6:
+				a.drawBitmap("laser1.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 7:
+				a.drawBitmap("triangle_block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 8:
+				a.drawBitmap("seed.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			}
+		}
 	}
-	for (y = 0; y < 12; y++) {
-		a.drawBitmap("block.bmp", buffer, x, y * BLOCK_SIZE, SCREEN_WIDTH);
-	}
-	a.drawBitmap("tutorial_building.bmp", buffer, x - 383, 80, SCREEN_WIDTH);
+
+	// 도어 그리기
+	a.drawBitmap("door.bmp", buffer, 2 * 19 * BLOCK_SIZE, BLOCK_SIZE - 20, SCREEN_WIDTH);
 
 	a.drawBitmap("bottom.bmp", buffer, 0, 480, SCREEN_WIDTH);
 
-	// 맵을 한 번에 출력 (루프 시작 전)
-
-	drawCharacter ac;
-
-
-
+	// 맵 출력
 	a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-	while (1) {
-		int preX = ac.x;
-		int preY = ac.y;
-		ac.gravity();
-
-
-
-		if (_kbhit()) {
-
-			ac.characterMove(buffer);
-		}
-
-
-		ac.characterEraese(preX, preY, buffer);
-		ac.characterDraw(ac.x, ac.y, buffer);
-
-		a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
-
-	}
-
+	getchar();
+	
 
 	return 0;
 
