db43575090b550fd2849f212192df8d75f7c71c6
psparrow3 <psparrow3@naver.com>
2024-11-09 21:41:36 +0900

    boss 이동 구현 및 짜잘한 오류 수정

--- 소설기/소설기/drawStage1.cpp
+++ 소설기/소설기/drawStage1.cpp
@@ -65,3 +65,43 @@ drawStage1::drawStage1()
 		a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
 	}
 }
+
+
+
+/*
+// stage1_past 배열 기반으로 맵을 그림
+	for (int y = 0; y < 12; y++) {
+		for (int x = 0; x < 20; x++) {
+			switch (stage1_past[y][x]) {
+			case 1:
+				a.drawBitmap("block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 2:
+				a.drawBitmap("lava2.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 3:
+				a.drawBitmap("movable_block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 4:
+				a.drawBitmap("key.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 5:
+				a.drawBitmap("button1.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE + 20, SCREEN_WIDTH);
+				break;
+			case 6:
+				a.drawBitmap("laser1.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 7:
+				a.drawBitmap("triangle_block.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			case 8:
+				a.drawBitmap("seed.bmp", buffer, 2 * x * BLOCK_SIZE, y * BLOCK_SIZE, SCREEN_WIDTH);
+				break;
+			}
+		}
+	}
+
+	// 도어 그리기
+	a.drawBitmap("door.bmp", buffer, 2 * 19 * BLOCK_SIZE, BLOCK_SIZE - 20, SCREEN_WIDTH);
+
+*/
--- 소설기/소설기/main.cpp
+++ 소설기/소설기/main.cpp
@@ -8,93 +8,72 @@
 #include "drawCharacter.h"
 #include "conio.h"
 #include "drawStage1.h"
+#include "Boss1.h"
 
-
-int stage_prologue[24][40]
-{
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,0,0,0},
-   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,9,9,2,2,2,2,2,2,2}
-};
 
 int main() {
-    draw a;
-    a.SetConsoleSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-    a.SetConsoleFontSize(1);
-    SetConsoleTitle(L"잃어버린 낙원");
+	draw a;
+	Boss1 b(18, 40);
+	a.SetConsoleSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	a.SetConsoleFontSize(1);
+	SetConsoleTitle(L"잃어버린 낙원");
 
-    // 커서 숨기기
-    HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    CONSOLE_CURSOR_INFO curCursorInfo;
-    GetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
-    curCursorInfo.bVisible = 0;
-    SetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
+	// 커서 숨기기
+	HANDLE hConsoleOut = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO curCursorInfo;
+	GetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
+	curCursorInfo.bVisible = 0;
+	SetConsoleCursorInfo(hConsoleOut, &curCursorInfo);
 
-    // 백그라운드(맵) 버퍼 생성
-    std::vector<char> buffer(SCREEN_WIDTH * SCREEN_HEIGHT, ' ');
-
-
-    // 맵 그리기 - 맵은 고정되어 있으므로 초기화 후 다시 그리지 않음
-    int x, y;
-    for (x = 0; x < SCREEN_WIDTH - BLOCK_SIZE * 10; x += BLOCK_SIZE * 2) {
-        a.drawBitmap("block.bmp", buffer, x, SCREEN_HEIGHT - BLOCK_SIZE, SCREEN_WIDTH);
-    }
-    for (y = SCREEN_HEIGHT - BLOCK_SIZE; y > 0; y -= BLOCK_SIZE) {
-        a.drawBitmap("block.bmp", buffer, x, y, SCREEN_WIDTH);
-    }
-    a.drawBitmap("tutorial_building.bmp", buffer, x - 383, SCREEN_HEIGHT - BLOCK_SIZE * 10, SCREEN_WIDTH);
-
-    drawCharacter ac;
+	// 백그라운드(맵) 버퍼 생성
+	std::vector<char> buffer(SCREEN_WIDTH * SCREEN_HEIGHT, ' ');
 
 
-    while (1) {
+	// 맵 그리기 - 맵은 고정되어 있으므로 초기화 후 다시 그리지 않음
+	
+	a.drawBitmap("bottom.bmp", buffer, 0, 480, SCREEN_WIDTH);
 
+	a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
 
-        if (ac.nextStage)
-            break;
+	int flag = 0;
+	int x = 18;
 
-        int preX = ac.x;
-        int preY = ac.y;
-        ac.characterErase(ac.x, ac.y, buffer);
-        // 캐릭터 위치 업데이트
-        if (ac.collision(stage_prologue, ac.x, ac.y) != 2 || ac.collision(stage_prologue, ac.x, ac.y) != 9) {
-            ac.gravity(stage_prologue, ac.x, ac.y);
-        }
+	while (1)
+	{
+		a.drawBitmap("empty_block.bmp", buffer, b.m_x, b.m_y, SCREEN_WIDTH);
 
+		if (!flag)
+		{
+			x--;
+			b.Boss1Move(x);
+			
+			if (x == 1)
+			{
+				flag = 1;
+			}
 
-        // 캐릭터 이동
-        ac.characterMove(stage_prologue, buffer);
-        if (ac.facingRight)
-            ac.characterRightDraw(ac.x, ac.y, buffer);
+			a.drawBitmap("block.bmp", buffer, b.m_x, b.m_y, SCREEN_WIDTH);
 
-        else
-            ac.characterLeftDraw(ac.x, ac.y, buffer);
+			a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
+		}
+		else
+		{
+			x++;
+			b.Boss1Move(x);
 
-        // 변경된 버퍼 내용만 화면에 출력
-        a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
+			if (x == 18)
+			{
+				flag = 0;
+			}
+			a.drawBitmap("block.bmp", buffer, b.m_x, b.m_y, SCREEN_WIDTH);
 
-    }
+			a.flushBuffer(buffer, SCREEN_WIDTH, SCREEN_HEIGHT);
+		}
 
-    return 0;
+		Sleep(200);
+	}
+
+	getchar();
+
+	return 0;
 }
